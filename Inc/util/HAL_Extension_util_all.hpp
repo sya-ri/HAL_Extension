72b9774bc67d75f2480f3f4228a7501ffe71e601
sya-ri <sya79lua@gmail.com>
2020-10-02 14:10:50 +0900

    angle_type, function_math_angle をインクルードするように変更

--- Inc/util/HAL_Extension_util_all.hpp
+++ Inc/util/HAL_Extension_util_all.hpp
@@ -2,6 +2,8 @@
 #define HAL_EXTENSION_UTIL_HPP
 
 #include "HAL_Extension_util_function.hpp"
+#include "HAL_Extension_util_angle_type.hpp"
 #include "HAL_Extension_util_timer_counter.hpp"
+#include "HAL_Extension_util_function_math_angle.hpp"
 
 #endif // HAL_EXTENSION_UTIL_HPP
