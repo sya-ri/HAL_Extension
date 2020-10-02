ed9afa13463cc801a828fd44871ec1b7cd9bc573
sya-ri <sya79lua@gmail.com>
2020-10-02 13:44:39 +0900

    AngleType を追加

---
+++ Inc/util/HAL_Extension_util_angle_type.hpp
@@ -0,0 +1,15 @@
+#ifndef HAL_EXTENSION_UTIL_ANGLE_TYPE_HPP
+#define HAL_EXTENSION_UTIL_ANGLE_TYPE_HPP
+
+#include "main.h"
+
+namespace halex {
+
+enum class AngleType: uint8_t {
+    Radian = 0,
+    Degree = 1
+};
+
+} // namespace halex
+
+#endif // HAL_EXTENSION_UTIL_ANGLE_TYPE_HPP
