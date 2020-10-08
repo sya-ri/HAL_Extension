849da6c26579fd45cef90e31d16bca303a42cd99
sya-ri <sya79lua@gmail.com>
2020-10-08 22:03:05 +0900

    BinaryArray, BinaryArrayRef をインクルードに追加

--- Inc/util/HAL_Extension_util_all.hpp
+++ Inc/util/HAL_Extension_util_all.hpp
@@ -4,6 +4,8 @@
 #include "HAL_Extension_util_function.hpp"
 #include "HAL_Extension_util_angle_type.hpp"
 #include "HAL_Extension_util_stop_watch.hpp"
+#include "HAL_Extension_util_binary_array.hpp"
+#include "HAL_Extension_util_binary_array_ref.hpp"
 #include "HAL_Extension_util_function_math_angle.hpp"
 
 #endif // HAL_EXTENSION_UTIL_HPP
