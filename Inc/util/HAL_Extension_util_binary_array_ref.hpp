12af446550d50238d7c270b4b2983e302531b1be
sya-ri <sya79lua@gmail.com>
2020-10-08 22:01:50 +0900

    クラス BinaryArrayRef を追加

---
+++ Inc/util/HAL_Extension_util_binary_array_ref.hpp
@@ -0,0 +1,41 @@
+#ifndef HAL_EXTENSION_UTIL_BINARY_ARRAY_REF_HPP
+#define HAL_EXTENSION_UTIL_BINARY_ARRAY_REF_HPP
+
+#include <array>
+#include "main.h"
+
+namespace halex {
+
+template <std::size_t ARRAY_SIZE>
+class BinaryArrayRef {
+private:
+    std::array<uint8_t, ARRAY_SIZE> &array;
+public:
+    BinaryArrayRef(std::array<uint8_t, ARRAY_SIZE> &array): array(array) {
+
+    }
+
+    void set(uint8_t index) noexcept {
+        array[index / 8] |= (1 << (index % 8));
+    }
+
+    void reset(uint8_t index) noexcept {
+        array[index / 8] &= ~(1 << (index % 8));
+    }
+
+    void write(uint8_t index, bool state) noexcept {
+        if (state) {
+            set(index);
+        } else {
+            reset(index);
+        }
+    }
+
+    bool read(uint8_t index) const noexcept {
+        return array[index / 8] & (1 << (index % 8));
+    }
+};
+
+} // namespace halex
+
+#endif // HAL_EXTENSION_UTIL_BINARY_ARRAY_REF_HPP
