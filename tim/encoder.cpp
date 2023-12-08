ce3f1ae7b49ee45195380e37151c2a7c37f4be4a
alcedo-taku <60389386+alcedo-taku@users.noreply.github.com>
2023-12-08 22:07:39 +0900

    fix(encoder): 値が飛ぶバグを修正 (#19)
    * fix(encoder): 値が飛ぶバグを修正
    値が counter period 分、飛ぶバグを修正
    
    * refactor(encoder): float 計算をなくし、処理を軽量化
    
    * refactor(encoder): overflow, underflow の条件式を修正
    
    * fix(encoder): 前回解決しきれていなかった値が飛ぶバグを修正
    値が counter period 分、飛ぶバグを修正
    原因： 想定していないタイミングで FLAG が立ち、 CLEAR されていたこと
    解決策： FLAG を使って overflow / underflow を検知するのを辞めた
    
    * refactor: cmath の include を削除

--- tim/encoder.cpp
+++ tim/encoder.cpp
@@ -37,13 +37,10 @@ void Encoder::update() noexcept {
     lastRawCount = rawCount;
     rawCount = __HAL_TIM_GET_COUNTER(htim);
     count += rawCount - lastRawCount;
-    if (__HAL_TIM_GET_FLAG(htim, TIM_FLAG_UPDATE)) {
-        __HAL_TIM_CLEAR_FLAG(htim, TIM_FLAG_UPDATE);
-        if (__HAL_TIM_IS_TIM_COUNTING_DOWN(htim)) {
-            count -= __HAL_TIM_GET_AUTORELOAD(htim);
-        } else {
-            count += __HAL_TIM_GET_AUTORELOAD(htim);
-        }
+    if (((int32_t) (lastRawCount - rawCount)) > ((int32_t) (__HAL_TIM_GET_AUTORELOAD(htim) / 2))) { // overflow
+        count += __HAL_TIM_GET_AUTORELOAD(htim);
+    } else if (((int32_t) (rawCount - lastRawCount)) > ((int32_t) (__HAL_TIM_GET_AUTORELOAD(htim) / 2))) { // underflow
+        count -= __HAL_TIM_GET_AUTORELOAD(htim);
     }
 }
 
